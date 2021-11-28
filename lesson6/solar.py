b959889d61d3a27572e7bd669225d924d05938c4
kelinsobw <93941013+kelinsobw@users.noreply.github.com>
2021-11-28 15:46:25 +0300

    Fix solar.py

--- lesson6/solar.py
+++ lesson6/solar.py
@@ -6,29 +6,25 @@ def area(s):
     while s_ost>=4:
         side_1=int(math.sqrt(s))
         panels.append(side_1)
-        side_ost=math.sqrt(s)- side_1
         prjamoug_1=[math.sqrt(s),math.sqrt(s)-side_1]
         prjamoug_2=[side_1,math.sqrt(s)-side_1]
-        s_ost=prjamoug_1[0]*prjamoug_1[1]+prjamoug_2[0]*prjamoug_2[1]
+        s_ost=s_ost-side_1**2
         s=s_ost
         print("  ")
         print("Выерезаем квадрат",side_1,"*",side_1)
         print("Остается 2 куска размерами",prjamoug_1,"и",prjamoug_2)
+        print("Общей площадью:", s_ost)
 
-    for i in range(int(s_ost)+1):
+    print("И добавляем ", int(s_ost),"панель(и) размерами 1*1.")
+    for i in range(int(s_ost)):
         s_ost-=1
         panels.append(1)
-        print(" ")
-        print("Выерезаем квадрат 1 * 1")
-        print("Остается 2 куска размерами",prjamoug_1,"и",prjamoug_2)
 
     print([i for i in panels])
     panels=[i*i for i in panels]
     return(panels)
 
-
-
-
+#изначально предполагаем что панель квадратная
 s=int(input("Введите площадь панели:"))
 panels=area(s)
-print(panels)
+print("Количество панелей:",len(panels),",размерами:",[str(i)+"кв.ярд." for i in panels])
