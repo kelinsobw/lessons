74b7d44269c14c9f9b2737058e18c716d62c5f72
kelinsobw <93941013+kelinsobw@users.noreply.github.com>
2021-12-07 21:24:41 +0300

    Add files via upload

--- lesson9/homework.py
+++ lesson9/homework.py
@@ -1,25 +1,4 @@
-class Car:
-    def __init__(self,marka,model,god,speed=0):
-        self.marka=marka
-        self.model=model
-        self.god=god
-        self.speed=speed
-
-    def speed_up(self):
-        self.speed+=5
-
-    def speed_down(self):
-        self.speed-=5
-
-    def speed_stop(self):
-        self.speed=0
-
-    def print_speed(self):
-        print(f'Скорость: {self.speed}')
-
-    def speed_back(self):
-        self.speed*=-1
-
+from class_car import Car
 
 
 if __name__=="__main__":
@@ -27,6 +6,5 @@ if __name__=="__main__":
 
     while my_car.speed<100:
         my_car.speed_up()
-        my_car.print_speed()
 
     my_car.print_speed()
