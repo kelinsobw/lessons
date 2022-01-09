cc0008c31c3397ddadfa80d5e77f746c0a3817f6
Siarhei Harelik <kelinso@mail.ru>
2022-01-09 20:28:49 +0300

    Я не совсем понял как правильно нужно связывать таблицы в запросах, так что чуть-чуть нагавнокодил

--- lesson12/homework.py
+++ lesson12/homework.py
@@ -1,3 +1,5 @@
+'''Я не совсем понял как правильно нужно связывать таблицы в запросах, так что чуть-чуть нагавнокодил'''
+
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
 from my_base import *
@@ -12,7 +14,7 @@ DB_ECHO = True
 
 
 def control():
-    choise=None
+    choise = None
     while choise != 0:
         print(f'Для продолжения работы выберите нужную опперацию:')
         print(f'1 -Добавить нового покупателя')
@@ -21,7 +23,52 @@ def control():
         print(f'4 -отобразить все товары купленные пользователем')
         print(f'0 -выход из программы')
         print(f'')
-        choise=int(input(f'Выберите необходимую опперацию...: '))
+        choise = int(input(f'Выберите необходимую опперацию...: '))
+
+        if choise == 1:     # новый покупатель
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            new_name = input(f'Введите Email: ')
+            customer = Customer(email=new_name)
+            session.add(customer)
+            session.commit()
+
+        if choise == 2:  # новый товар
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            new_name = input(f'Введите название продукта: ')
+            new_price = input(f'Введите цену: ')
+            product = Product(name=new_name, price=new_price)
+            session.add(product)
+            session.commit()
+
+        if choise == 3:  # новая покупка
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            email = input("Введите email пользователя")
+            human = session.query(Customer).filter(Customer.email == email).first()
+            product = input("Введите название продукта")
+            product = session.query(Product).filter(Product.name == product).first()
+            cout_x = int(input("Введите количесво продукта"))
+            purchare = Purchase(id_pr=product.id_product, id_cu=human.id_customer, cout=cout_x,
+                                receipt=cout_x*product.price, date=datetime.now())
+            session.add(purchare)
+            session.commit()
+
+        if choise == 4:  # просмотр покупок определенного покупателя
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            email = input("Введите email пользователя")
+            user = session.query(Customer).filter(Customer.email == email).first()
+            user = user.id_customer
+            purchares = session.query(Purchase).filter(Purchase.id_cu == user).all()
+            for i in purchares:
+                name_product = session.query(Product).filter(Product.id_product == Purchase.id_pr).first()
+                name_product = name_product.name
+                print(f'Купил товар {name_product} на сумму {i.receipt} в количестве {i.cout} шт. {i.date}')
+
+        if choise == 0:     # выход
+            break
 
 
 if __name__ == "__main__":
@@ -31,9 +78,4 @@ if __name__ == "__main__":
     if not database_exists(engine.url):
         create_database(engine.url)
     Base.metadata.create_all(engine)
-    request = None
     control()
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    session.commit()
--- lesson12/my_base.py
+++ lesson12/my_base.py
@@ -1,27 +1,30 @@
-from sqlalchemy import Integer, String, Float, Column, Table, MetaData, ForeignKey, DateTime
+from sqlalchemy import Integer, String, Float, Column, ForeignKey, DateTime, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import relationships
 
 
 Base = declarative_base()
+metadata = MetaData()
+
+class Customer(Base):
+    __tablename__ = "customer"
+    id_customer = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False, unique=True)
 
 
-class User(Base):
-    __tablename__ = "users"
-    id_user= Column(Integer, primary_key=True)
-    email = Column(String)
-
-
-class Prouct(Base):
+class Product(Base):
     __tablename__ = "products"
     id_product = Column(Integer, primary_key=True)
-    name = Column(String)
-    price = Column(Float)
+    name = Column(String, nullable=False, unique=True)
+    price = Column(Float, nullable=False)
 
 
 class Purchase(Base):
     __tablename__ = "purchare"
     id_purchare = Column(Integer, primary_key=True)
-    user = Column(Integer, ForeignKey(User.id_user), nullable=False)
-    product = Column(Integer, ForeignKey(Prouct.id_product), nullable=False)
+    id_pr = Column(Integer, ForeignKey(Product.id_product), nullable=False)
+    id_cu = Column(Integer, ForeignKey(Customer.id_customer), nullable=False)
     cout = Column(Integer)
+    receipt = Column(Float)
     date = Column(DateTime)
+
